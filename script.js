f724b394a18e577b0530e3e673bddd80530e8d4b
Alex Hallett <215055353+SirGhostdog@users.noreply.github.com>
2026-04-16 23:53:24 -0500

    Add DOM funcitonality. Need to finish all boxes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const weatherData = []
 
-function weatherPromise() {
-    let location = '96093'
+function weatherPromise(location) {
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/?key=4CCU3Y3NC2NWGPLQ9R6Z52RLU`)
         .then(function(response){
             return response.json()
@@ -9,22 +8,36 @@ function weatherPromise() {
         .then(function(response){
             const rawData = response
             weatherData.push(rawData.currentConditions)
+            addItemToPage()
         })
         .catch(function(error){
             console.log(error)
         })
 }
 
+
+
 (function formData(){
+
     const form = document.querySelector('form')
+
     form.addEventListener('submit', (event) => {
+
+        weatherData.length = 0
         event.preventDefault()
         const data = new FormData(form)
         const address = data.get('addinput')
-        console.log(address)
+        weatherPromise(address)
+        // console.log(weatherData)
     })
 })();
 
-// weatherPromise()
+function addItemToPage() {
 
-console.log(weatherData)
+    if (weatherData.length === 0) return;
+
+    const current = weatherData[0];
+
+    const tempEl = document.getElementById('temperature')
+    if (tempEl) tempEl.textContent = current.temp
+}
